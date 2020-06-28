575e1bf7e9df475d3fb1fd45bc2fedf05bd215da
Tom McClintock <tmcclintock89@gmail.com>
2020-06-27 22:57:06 -0700

    Adding a setup file.

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+Setup file for making violin plots.
+"""
+from distutils.core import setup
+
+setup(
+    name="bvp",
+    version="0.1",
+    description="Good violin plots.",
+    author="Thomas McClintock",
+    author_email="thmsmcclintock@gmail.com",
+    url="https://github.com/tmcclintock/BetterViolinPlots",
+    packages=["bvp"],
+)
