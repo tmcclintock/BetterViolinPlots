048de84685d271e8ed1ab734cf990cd527d93c13
Tom McClintock <tmcclintock89@gmail.com>
2020-07-02 18:16:41 -0700

    Got the analytic distributions working and getting ready for KDEs.

--- bvp/plots.py
+++ bvp/plots.py
@@ -18,19 +18,33 @@ def _xy_order(domain: List, dist: List, vertical_violin: bool) -> Dict:
 
 
 def analytic_violin(
-    dists: List,
+    distributions: List,
     positions: Optional[List[int]] = None,
     axis: Optional["mpl.axes.Axes"] = None,
     vertical_violins: bool = True,
-    plot_kwargs: Optional[Union[Dict[str, Any], List[Dict[str, Any]]]] = {},
+    plot_kwargs: Optional[Union[Dict[str, Any], List[Dict[str, Any]]]] = {
+        "color": "black",
+    },
 ) -> Tuple[mpl.figure.Figure, mpl.axes.Axes]:
     """
-    Create violin plots of the analytic distributions.
+    Create violin plots of analytic distributions.
 
-    Note: can accept either discrete or continuous distributions.
+    .. note::
+
+       can accept either discrete or continuous distributions.
+
+    .. note::
+
+       the default plot keywords are
+
+       .. code-bloc:: python
+
+          plot_kwargs = {
+             "color": "black",
+          }
 
     Args:
-        dists (List): analytic distributions
+        distributions (List): analytic distributions
         axis (mpl.axes.Axes): axis to use for plotting, default `None`
         vertical_violins (bool): flag to indicate orientation
         plot_kwargs (Dict or List): if Dict, a dictionary of keyword-value
@@ -42,15 +56,14 @@ def analytic_violin(
     else:
         fig = axis.get_figure()
 
-    if plot_kwargs is not None:
-        if isinstance(plot_kwargs, list):
-            assert len(dists) == len(plot_kwargs)
+    if isinstance(plot_kwargs, list):
+        assert len(distributions) == len(plot_kwargs)
 
     if positions is not None:
-        assert len(dists) == len(positions)
+        assert len(distributions) == len(positions)
     else:
         # Horizontal positions of the centers of the violins
-        positions = np.arange(0, len(dists))
+        positions = np.arange(0, len(distributions))
 
     # Center positions between integers
     if vertical_violins:
@@ -59,7 +72,7 @@ def analytic_violin(
         axis.set_ylim(positions.min() - 0.5, positions.max() + 0.5)
 
     # Loop over all distributions and draw the violin
-    for i, d in zip(positions, dists):
+    for i, d in zip(positions, distributions):
         interval = d.interval(0.99999)  # 5-sigma
 
         if isinstance(plot_kwargs, list):
@@ -77,7 +90,7 @@ def analytic_violin(
                 for xi, yi in zip(x, y):
                     # right side
                     axis.plot(
-                        _xy_order(
+                        *_xy_order(
                             [xi, xi + 1],
                             [i - yi * scale, i - yi * scale],
                             vertical_violins,
@@ -86,7 +99,7 @@ def analytic_violin(
                     )
                     # left side
                     axis.plot(
-                        _xy_order(
+                        *_xy_order(
                             [xi, xi + 1],
                             [i + yi * scale, i + yi * scale],
                             vertical_violins,
@@ -95,16 +108,15 @@ def analytic_violin(
                     )
             elif isinstance(d.dist, rv_continuous):
                 x = np.linspace(min(interval), max(interval), 100)
-                y = d.dist.pdf(x)
+                y = d.pdf(x)
                 scale = 0.4 / y.max()
                 # left side
                 axis.plot(
-                    _xy_order(x, i - y * scale, vertical_violins), **kwargs,
+                    *_xy_order(x, i - y * scale, vertical_violins), **kwargs,
                 )
-                print(_xy_order(x, i - y * scale, vertical_violins))
                 # right side
                 axis.plot(
-                    _xy_order(x, i + y * scale, vertical_violins), **kwargs,
+                    *_xy_order(x, i + y * scale, vertical_violins), **kwargs,
                 )
             else:  # need to do random draws
                 raise NotImplementedError(
@@ -116,6 +128,32 @@ def analytic_violin(
             )  # pragma: no cover
 
     return fig, axis
+
+
+def kde_violin():
+    """
+    Create violin plots of Gaussian kernel density estimations (KDE)
+    of points.
+
+    .. note::
+
+       the default plot keywords are
+
+       .. code-bloc:: python
+
+          plot_kwargs = {
+             "color": "black",
+          }
+
+    Args:
+        points (List): analytic distributions
+        axis (mpl.axes.Axes): axis to use for plotting, default `None`
+        vertical_violins (bool): flag to indicate orientation
+        plot_kwargs (Dict or List): if Dict, a dictionary of keyword-value
+            pairs to pass to each plot routine. If List, it is a list of
+            Dict objects to pass, one for each plot routine
+    """
+    pass
 
 
 def point_violin():
